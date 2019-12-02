27d1070a2ac49c53959153713fe8909190b7c954
comdet <listzone@hotmail.com>
2019-12-02 14:03:07 +0700

    update config information file.

--- config.js
+++ config.js
@@ -2,15 +2,17 @@ module.exports = {
   name: "kbide-extension-pack",
   title: "KBIDE Extensions Pack",
   description: "KBIDE Tools (Packed) Ex. Serial Monitor, Developer code preview, C/C++ Code auto format",
-  auther: "Maker Asia Co.,Ltd.",
-  website: "kbide.org",
-  git: "",
+  keywords : "extension,pack,KBIDE,serial monitor",
+  author: "Maker Asia Co.,Ltd.",
+  website: "https://kbide.org",
+  git: "https://github.com/cmmc-kbide/kbide-extension-pack/",
   image: "/static/extension.jpg",
   version: "1.0.0",
   component: [
     "serial-monitor",
     "actionbar-serial"
   ],
+  dependencies : [],
   data: {
     loaded: false, //this will automatic set to 'true' if this pacakage loaded to IDE
   },
